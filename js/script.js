237acf33fa7d0d7f85b09aab867a81d091337ca8
habitual69 <admin@vpms.xyz>
2023-09-03 20:08:22 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+let currentPage = 1;
+const itemsPerPage = 29;
+const searchBtn = document.getElementById('search-btn');
+const searchInput = document.getElementById('search-input');
+const imageGrid = document.getElementById('image-grid');
+const prevBtn = document.getElementById('prev-page');
+const nextBtn = document.getElementById('next-page');
+const currentPageSpan = document.getElementById('current-page');
+const totalPagesSpan = document.getElementById('total-pages');
+const orient = document.getElementById('orientation');
+
+searchBtn.addEventListener('click', () => {
+    currentPage = 1;  // Reset to the first page on a new search
+    fetchImages(searchInput.value, currentPage);
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        fetchImages(searchInput.value, currentPage);
+    }
+    updatePaginationButtons();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentPage++;
+    fetchImages(searchInput.value, currentPage);
+    updatePaginationButtons();
+});
+
+const rippleLoader = document.getElementById('ripple-loader');
+
+function showRippleLoader() {
+    rippleLoader.style.display = 'inline-block';
+}
+
+function hideRippleLoader() {
+    rippleLoader.style.display = 'none';
+}
+
+function fetchImages(query, page) {
+    showRippleLoader();  // Show ripple loader before starting fetch
+    fetch(`http://uns.vpms.xyz/v1/search?query=${query}&per_page=${itemsPerPage}&page=${page}&orientation=${orient.value}`)
+        .then(response => response.json())
+        .then(data => {
+            displayImages(data);
+            currentPageSpan.textContent = currentPage;
+            totalPagesSpan.textContent = 100;  // Adjust as needed
+            updatePaginationButtons();
+            hideRippleLoader();  // Hide ripple loader after images are loaded
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            hideRippleLoader();  // Also hide ripple loader on error
+        });
+}
+
+
+function displayImages(images) {
+    imageGrid.innerHTML = '';  // Clear existing images
+
+    const generateImageHtml = (image) => {
+        const imageUrl = image.urls.regular;
+        const rawUrl = image.urls.raw;
+        return `
+            <div class=" container mx-auto image-container rounded-lg">
+                <img src="${imageUrl}" alt="${image.alt_description || 'No Description'}">
+                <div class="overlay">
+                    <a href="${rawUrl}" target="_blank" download>
+                        <i class="fa-light fa-download" style="color: #ffffff;"></i>
+                    </a>
+                </div>
+            </div>
+        `;
+    }
+    
+    
+
+    // Generate HTML for all images and append to the imageGrid
+    imageGrid.innerHTML = images.map(generateImageHtml).join('');
+}
+
+function updatePaginationButtons() {
+    // Disabling the 'prev' button if on the first page
+    prevBtn.disabled = (currentPage <= 1);
+    
+    // Disabling the 'next' button if on the last page. 
+    // This assumes that the total pages are updated in the totalPagesSpan
+    nextBtn.disabled = (currentPage >= parseInt(totalPagesSpan.textContent));
+}
---
+++ readme.md
@@ -0,0 +1,60 @@
+# Image Search Application (Educational Project)
+
+This is a demonstration and educational project that allows users to search for high-quality images using a reverse-engineered API based on Unsplash. This project is not affiliated with Unsplash and is designed purely for learning purposes.
+
+![Image Search App Screenshot](screenshot.png)
+
+
+## Features
+
+- User-friendly search interface.
+- Responsive grid layout for image display.
+- Pagination controls for easy navigation.
+- Direct download option for high-resolution images.
+- Smooth ripple loading animation during fetch.
+
+## Prerequisites
+
+- Modern web browser (Chrome, Firefox, Safari, Edge).
+
+## Installation and Setup
+
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/your-username/image-search-app.git
+   ```
+
+2. **Navigate into the directory**:
+   ```bash
+   cd image-search-app
+   ```
+
+3. **Launch using a live server**:
+   Utilize tools such as `Live Server` in VSCode or just open `index.html` in a web browser.
+
+## Usage
+
+1. Input your desired keyword in the search bar.
+2. Click "Search" or simply press Enter.
+3. Scroll through images and navigate with pagination controls.
+4. Tap on an image's download icon to get the high-resolution variant.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- Pure JavaScript
+- Reverse-engineered API inspired by Unsplash
+
+## Contribution
+
+Contributions, insights, and suggestions are welcome. Please keep in mind that this project is for educational purposes. Fork the repository and create a pull request if you have improvements or fixes.
+
+## Educational Disclaimer
+
+This application is created purely for educational and demonstration purposes. It uses a reverse-engineered API inspired by Unsplash. The intention is to provide a learning platform for individuals to understand web development techniques and not for commercial use. Respect all relevant terms of service and licensing when reviewing and using this software. This project is not affiliated, associated, authorized, endorsed by, or in any way officially connected with Unsplash or any of its subsidiaries or affiliates.
+
+## Acknowledgements
+
+Special thanks to the original design and inspiration from [Unsplash](https://unsplash.com). This project is an educational representation and not meant for commercial distribution.
+
